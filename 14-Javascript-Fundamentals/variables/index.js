96eefa1a4cb0003e165d3c98bdf261b0842d3f7f
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2025-05-27 21:05:13 +0200

    Remove unused alert statements and add HTML structure for JavaScript fundamentals

---
+++ 14-Javascript-Fundamentals/variables/index.js
@@ -0,0 +1,26 @@
+// window.alert("Hello world");
+// alert("ehhw");
+
+// console.log(typeof(23));
+// console.log(typeof("Angela"));
+
+// var myName = prompt("What is your name? ");
+// alert(`Hello, ${myName}!`)
+
+
+// Swapping variables
+
+function test(){
+    var a = "3";
+    var b = "8";
+
+    var c =a;
+    a=b;
+    b=c;
+
+    console.log("a is " + a);
+    console.log("b is " + b);
+    
+}
+
+test();
