a0fcc64fb4571adec5ce4a6208f099d2dfefd3c5
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2025-05-30 11:15:41 +0200

    Add BMI calculator challenge

--- 14-Javascript-Fundamentals/variables/index.js
+++ 14-Javascript-Fundamentals/variables/index.js
@@ -114,13 +114,41 @@ Example Output
 You have 12410 days, 1768 weeks, and 408 months left.
 */
 
-function lifeInWeeks(age){
-    var remainingYears = 90 -age;
-    var days = remainingYears*365;
-    var weeks =  remainingYears * 52;
-    var months = remainingYears * 12;
+// function lifeInWeeks(age){
+//     var remainingYears = 90 -age;
+//     var days = remainingYears*365;
+//     var weeks =  remainingYears * 52;
+//     var months = remainingYears * 12;
     
-    console.log(`You have ${days} days, ${weeks} weeks, and ${months} months left.`);
+//     console.log(`You have ${days} days, ${weeks} weeks, and ${months} months left.`);
+// }
+
+// lifeInWeeks(56);
+
+/*
+
+BMI Calculator Challenge
+Create a BMI calculator using JavaScript functions. 
+
+The Body Mass Index (BMI) is a way of estimating the amount of body fat. It's used in medicine to calculate risk of heart disease. You can calculate it using the formula below, where weight divided by height squared.
+Your challenge is to create a function that takes weight and height as inputs and gives the calculated BMI value as an output. The output should be rounded to the nearest whole number.
+The first parameter should be the weight and the second should be the height.
+*/
+//Create your function below this line.
+//The first parameter should be the weight and the second should be the height.
+
+function bmiCalculator(weight, height){
+    return Math.round(weight/Math.pow(height,2));
 }
 
-lifeInWeeks(56);
+
+/* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+
+var bmi = bmiCalculator(65, 1.8); 
+
+bmi should equal 20 when it's rounded to the nearest whole number.
+
+*/
+
+var bmi = bmiCalculator(65, 1.8); 
+console.log(bmi);
