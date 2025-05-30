7fde6b19388f708251356286932c4eeb60d733ec
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2025-05-30 07:49:20 +0200

    Add sections covered

--- 14-Javascript-Fundamentals/variables/index.js
+++ 14-Javascript-Fundamentals/variables/index.js
@@ -1,6 +1,8 @@
 // window.alert("Hello world");
 // alert("ehhw");
 
+// const { use } = require("react");
+
 // console.log(typeof(23));
 // console.log(typeof("Angela"));
 
@@ -24,3 +26,29 @@ function test(){
 }
 
 test();
+
+
+// var tweet = prompt("Express yourself! ");
+// var tweetCount = tweet.length
+
+// alert(" You have written "+ (tweetCount) + " characters, you have"+ 140 - tweetCount +" characters left.")
+// alert(tweet.slice(0,140))
+
+
+// var username = "Angela";
+//  var usernameSliced = username.slice(1,3);
+
+// alert(usernameSliced);
+
+// var username = prompt("Enter your Name: ")
+// var first_letter = username.slice(0,1).toUpperCase();
+// alert(first_letter+username.slice(1, username.length).toLowerCase());
+
+// alert(9%6);
+// alert(3 + 5*2);
+
+var dogAge = prompt("What is your dog age?")
+
+var humanAge = (dogAge - 2) *4 + 21;
+
+alert("Your dog age in human age is " + humanAge)
