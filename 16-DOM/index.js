c2bbdf528e06054e67eb9be5272f327674477559
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2025-06-06 22:07:39 +0200

    Add more DOM manipulation synthax

--- 16-DOM/index.js
+++ 16-DOM/index.js
@@ -1,6 +1,7 @@
-var heading = document.querySelector("h1")
+const heading = document.querySelector("h1")
 heading.innerHTML = "Good Bye!";
 heading.style.color = "red";
+heading.classList.add('huge')
 
 const input = document.querySelector('input');
 input.click();
@@ -8,3 +9,36 @@ input.click();
 const listItem = document.querySelector('ul').lastElementChild;
 
 listItem.innerHTML = "Dany";
+
+// const lists = document.getElementsByTagName('li');
+const lists = document.getElementsByClassName('list')
+
+console.log(lists);
+
+const title = document.getElementById('title')
+console.log(title);
+
+
+const lis = document.querySelector("#title");
+
+console.log(lis);
+
+const link = document.querySelector('li a');
+console.log(link);
+
+// Quering
+const links = document.querySelectorAll('#list item');
+console.log(links);
+
+
+const anchor = document.querySelector('li a');
+anchor.style.color ='red';
+anchor.style.fontSize= '2rem'
+
+const btn =document.querySelector('button')
+btn.style.backgroundColor= 'yellow';
+btn.style.border ='none'
+btn.style.padding = '1rem 2rem'
+
+btn.classList.add('invisible');
+btn.classList.remove('invisible')
