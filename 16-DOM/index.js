a716a7b5b7e38b6ddd7652e50c36899bc5d328ab
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2025-06-09 13:41:24 +0200

    Enhance HTML and JavaScript
    It will add emphasis to heading and list item, update anchor link, and improve text manipulation logging

--- 16-DOM/index.js
+++ 16-DOM/index.js
@@ -8,7 +8,7 @@ input.click();
 
 const listItem = document.querySelector('ul').lastElementChild;
 
-listItem.innerHTML = "Dany";
+listItem.innerHTML = "<em>Dany</em>";
 
 // const lists = document.getElementsByTagName('li');
 const lists = document.getElementsByClassName('list')
@@ -28,7 +28,7 @@ console.log(link);
 
 // Quering
 const links = document.querySelectorAll('#list item');
-console.log(links);
+// console.log(links);
 
 
 const anchor = document.querySelector('li a');
@@ -42,3 +42,18 @@ btn.style.padding = '1rem 2rem'
 
 btn.classList.add('invisible');
 btn.classList.remove('invisible')
+
+// text manipulation
+
+const text = document.querySelector('p').innerHTML;
+const p = document.querySelector('p').textContent
+console.log(text);
+console.log(p);
+
+// Manipulating HTML Element Attributes 
+const anchors = document.querySelector('a');
+// const href = anchors.getAttribute('href');
+const href = anchors.setAttribute('href','https://www.bing.com');
+
+console.log(anchors,href);
+ 
