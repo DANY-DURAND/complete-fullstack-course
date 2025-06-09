e998f590719582ea0a0ba0b4e055b2734f572ee3
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2025-06-09 14:41:26 +0200

    Implement dice rolling functionality with dynamic image updates and win conditions

---
+++ 17-The-Dicee-Challenge/index.js
@@ -0,0 +1,30 @@
+const img1 = document.querySelector('.img1');
+// img1.setAttribute('src','./images/dice1.png');
+
+const img2 = document.querySelector('.img2');
+
+
+const refresh = document.querySelector('h1');
+
+console.log(refresh)
+
+function rollDice() {
+    var rand1 = Math.floor(Math.random() *6)+1;
+    // console.log(rand1);
+    img1.setAttribute('src',`./images/dice${rand1}.png`);
+
+
+    var rand2 = Math.floor(Math.random() *6)+1;
+    // console.log(rand2);
+    img2.setAttribute('src',`./images/dice${rand2}.png`);
+
+    if (rand1 > rand2) {
+        refresh.textContent = 'Player 1 wins🚩';
+    } else if (rand1 < rand2) {
+        refresh.textContent = 'Player 2 wins🚩';
+    }else{
+        refresh.textContent = `Draw`;
+    }
+
+}
+rollDice();
