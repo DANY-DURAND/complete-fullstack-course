70da759e491c56788fbe9ed279643e0b3005c7ab
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2025-06-10 11:47:54 +0200

    Update event listener to use play function for drum button clicks

--- 18-Advanced-JavaScript-and-DOM-Manipulation/index.js
+++ 18-Advanced-JavaScript-and-DOM-Manipulation/index.js
@@ -3,11 +3,19 @@ const btn = document.querySelectorAll('.drum');
 
 btn.forEach(element => {
     
-    element.addEventListener('click',handleClick)
+    element.addEventListener('click',play);
+
 });
 
-function handleClick(){
-    alert('I got clicked!');
+// function handleClick(){
+//     // alert('I got clicked!');
+    
+// }
+
+
+// function addSound()
+
+function play(){
+    var audio = new Audio('./sounds/tom-1.mp3');
+    audio.play();
 }
-
-
