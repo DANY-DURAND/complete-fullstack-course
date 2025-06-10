3cd2aa23fcd709b3e8cb1cacf70429d76f436f2f
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2025-06-10 14:24:44 +0200

    Refactor drum button event handling and add missing sound cases

--- 18-Advanced-JavaScript-and-DOM-Manipulation/index.js
+++ 18-Advanced-JavaScript-and-DOM-Manipulation/index.js
@@ -1,11 +1,13 @@
 const btn = document.querySelectorAll('.drum');
 
 
-btn.forEach(element => {
+// btn.forEach(element => {
     
-    element.addEventListener('click',play);
+//     element.addEventListener('click',play);
 
-});
+    
+
+// });
 
 // function handleClick(){
 //     // alert('I got clicked!');
@@ -15,7 +17,45 @@ btn.forEach(element => {
 
 // function addSound()
 
-function play(){
-    var audio = new Audio('./sounds/tom-1.mp3');
+
+for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click", function(){
+        // console.log(this.innerHTML);
+        // this.classList.add('clicked');
+
+
+        switch (this.innerHTML) {
+            case 'w':
+                play('./sounds/tom-1.mp3')
+                break;
+            case 'a':
+                play('./sounds/tom-2.mp3');
+                break;
+            case 's':
+                play('./sounds/tom-3.mp3')
+                break;
+            case 'd':
+                play('./sounds/tom-4.mp3');
+                break;
+            case 'j':
+                play('./sounds/snare.mp3')
+                break;
+            case 'k':
+                play('./sounds/crash.mp3');
+                break;
+            case 'l':
+                play('./sounds/kick-bass.mp3');
+                break
+            default:
+                break;
+        }
+    })
+}
+
+
+
+
+function play(path){
+    var audio = new Audio(path);
     audio.play();
 }
