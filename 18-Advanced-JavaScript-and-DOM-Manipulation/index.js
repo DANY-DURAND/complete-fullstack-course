87edbc2151e1e2deda7dff4d2fad4a9879253000
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2025-06-11 13:16:58 +0200

    Refactor drum button event handling to use makeSound function for sound playback

--- 18-Advanced-JavaScript-and-DOM-Manipulation/index.js
+++ 18-Advanced-JavaScript-and-DOM-Manipulation/index.js
@@ -1,61 +1,59 @@
-const btn = document.querySelectorAll('.drum');
-
+const btn = document.querySelectorAll(".drum");
 
 // btn.forEach(element => {
-    
-//     element.addEventListener('click',play);
 
-    
+//     element.addEventListener('click',play);
 
 // });
 
-// function handleClick(){
-//     // alert('I got clicked!');
-    
-// }
+/*function handleClick(){
+ alert('I got clicked!');
 
-
-// function addSound()
-
+}*/
 
 for (let i = 0; i < btn.length; i++) {
-    btn[i].addEventListener("click", function(){
-        // console.log(this.innerHTML);
-        // this.classList.add('clicked');
-
-
-        switch (this.innerHTML) {
-            case 'w':
-                play('./sounds/tom-1.mp3')
-                break;
-            case 'a':
-                play('./sounds/tom-2.mp3');
-                break;
-            case 's':
-                play('./sounds/tom-3.mp3')
-                break;
-            case 'd':
-                play('./sounds/tom-4.mp3');
-                break;
-            case 'j':
-                play('./sounds/snare.mp3')
-                break;
-            case 'k':
-                play('./sounds/crash.mp3');
-                break;
-            case 'l':
-                play('./sounds/kick-bass.mp3');
-                break
-            default:
-                break;
-        }
-    })
+  btn[i].addEventListener("click", function () {
+    // console.log(this.innerHTML);
+    // this.classList.add('clicked');
+    makeSound(this.innerHTML);
+  });
 }
 
+function play(path) {
+  var audio = new Audio(path);
+  audio.play();
+}
 
+document.addEventListener("keydown", (event) => {
+//   alert("Key was pressed!");
+//   console.log(event.key);
+  makeSound(event.key);
+});
 
-
-function play(path){
-    var audio = new Audio(path);
-    audio.play();
+function makeSound(key) {
+  switch (key.toLowerCase()) {
+    case "w":
+      play("./sounds/tom-1.mp3");
+      break;
+    case "a":
+      play("./sounds/tom-2.mp3");
+      break;
+    case "s":
+      play("./sounds/tom-3.mp3");
+      break;
+    case "d":
+      play("./sounds/tom-4.mp3");
+      break;
+    case "j":
+      play("./sounds/snare.mp3");
+      break;
+    case "k":
+      play("./sounds/crash.mp3");
+      break;
+    case "l":
+      play("./sounds/kick-bass.mp3");
+      break;
+    default:
+      console.log(this.innerHTML);
+  }
 }
