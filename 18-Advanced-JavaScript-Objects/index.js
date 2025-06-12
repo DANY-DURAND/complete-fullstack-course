071d47a23a8e12dad4191ca8118e21123e00e76e
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2025-06-12 20:19:25 +0200

    Add toggle functionality for list items to mark as done

--- 18-Advanced-JavaScript-Objects/index.js
+++ 18-Advanced-JavaScript-Objects/index.js
@@ -117,3 +117,17 @@ function removeNeg(numbers, callback) {
     }
     return myArr;
 }
+
+const listItems = document.querySelectorAll('li');
+
+function toggleDone(e) {
+    if (!e.target.className) {
+        e.target.className = 'done';
+    } else {
+        e.target.className = '';
+    }
+}
+
+listItems.forEach((item)=>{
+    item.addEventListener('click', toggleDone)
+})
