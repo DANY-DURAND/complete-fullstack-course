4de44cf7b52485383615f5727b4d2fe5f7ea0f30
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2025-06-13 12:07:01 +0200

    Add initial HTML structure, JavaScript functionality, and CSS

--- 19-jQuery/index.js
+++ 19-jQuery/index.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
-    $("h1").css('color', 'red');
+    // $("h1").css('color', 'green');
+
+
+    // Adding Classes for separation for concerns 
+    $('h1').addClass("big-title");
+    $('h1').removeClass("big-title");
+    
+
+    $('button').css('display', 'block');
+    $('button').css('margin', '1rem');
+    console.log(
+        $('button').css('margin')
+    );
+
+    // Text Maninpulation
+    $('h1').text("Bye");
+
+    $('button').text("Don't Click me");
+
+    $('button').html("<em>Don't Click me</em>");
+
+
+    // Attribute manipulation
+    console.log($('button').attr("style"));
+    $('a').attr('href', 'https://www.yahoo.com')
+
+
 })
