185f7be0d42af5a4730333cf4b09c70b5635d9b3
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2025-06-16 20:23:52 +0200

    Refactor button click handling and streamline user input logic

--- 20-Simon-Game-Challenge/game.js
+++ 20-Simon-Game-Challenge/game.js
@@ -4,11 +4,20 @@
 //     $('h1')
 // });
 
-const userClickedPattern = [];
 
 const buttonColours = ["red", "blue", "green", "yellow"];
 const gamePattern = [];
 
+const userClickedPattern = [];
+
+$('.btn').click(function () { 
+        const userChoosenColour = $(this).attr('id');
+        // console.log(userChoosenColour);
+        userClickedPattern.push(userChoosenColour);
+
+        playSound(userChoosenColour);
+        animatePress(userChoosenColour)
+});
 
 function nextSequence() {
     const randomNumber = Math.floor(Math.random()*4);
@@ -20,16 +29,7 @@ function nextSequence() {
     $('#'+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
     playSound(randomChosenColour);
-
-    $('.btn').click(function () { 
-        const userChoosenColour = $(this).attr('id');
-        console.log(userChoosenColour);
-
-        userClickedPattern.push(userChoosenColour);
-
-        playSound(userChoosenColour);
-    });
-
+    // animatePress(randomChosenColour)
 }
 function playSound(name) {
     const audio = new Audio(`sounds/${name}.mp3`);
@@ -38,13 +38,11 @@ function playSound(name) {
 
 
 function animatePress(currentColour) {
-    $(`#${currentColour}`).click(function(){
-
-        $(this).addClass('pressed');
+    $(`#${currentColour}`).addClass('pressed');
         
         setTimeout(() => {
-            $(this).removeClass('pressed');
+            $(`#${currentColour}`).removeClass('pressed');
         }, 100);
-
-    });
 }
+
+$(this).keydown(nextSequence);
