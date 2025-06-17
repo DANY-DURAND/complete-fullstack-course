4b0b5f1330f3b31d8ae9b52e6e97ae6934d797c2
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2025-06-17 16:26:44 +0200

    Implement game start logic and level tracking on keypress

--- 20-Simon-Game-Challenge/game.js
+++ 20-Simon-Game-Challenge/game.js
@@ -10,16 +10,43 @@ const gamePattern = [];
 
 const userClickedPattern = [];
 
-$('.btn').click(function () { 
-        const userChoosenColour = $(this).attr('id');
-        // console.log(userChoosenColour);
-        userClickedPattern.push(userChoosenColour);
 
-        playSound(userChoosenColour);
-        animatePress(userChoosenColour)
+//You'll need a way to keep track of whether if the game has started or not, so you only call nextSequence() on the first keypress.
+var started = false;
+
+// Create a new variable called level and start at level 0.
+var level = 0;
+
+// Use jQuery to detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence().
+
+$(document).keypress(()=>{
+    if (!started) {
+        // The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
+
+        $('#level-title').text('Level ' + level);
+        nextSequence();
+        started= true;
+    }
+});
+
+
+$('.btn').click(function () { 
+    var userChoosenColour = $(this).attr('id');
+    // console.log(userChoosenColour);
+    userClickedPattern.push(userChoosenColour);
+    
+    playSound(userChoosenColour);
+    animatePress(userChoosenColour)
 });
 
 function nextSequence() {
+
+    // Inside nextSequence(), increase the level by 1 every time nextSequence() is called.
+    level++; 
+
+    // Inside nextSequence(), update the h1 with this change in the value of level.
+  $("#level-title").text("Level " + level);
+
     const randomNumber = Math.floor(Math.random()*4);
     // console.log(randomNumber);
 
@@ -45,4 +72,4 @@ function animatePress(currentColour) {
         }, 100);
 }
 
-$(this).keydown(nextSequence);
+
