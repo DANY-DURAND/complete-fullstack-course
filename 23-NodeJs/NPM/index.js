061c1987f7aaeaa7abcc04d66b757bfc4c8dcab3
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2025-06-18 16:10:53 +0200

    Add simple example of npm package import

---
+++ 23-NodeJs/NPM/index.js
@@ -0,0 +1,12 @@
+import {randomSuperhero} from "superheroes";
+
+import generateName from"sillyname";
+
+var sillyName = generateName();
+
+console.log(sillyName);
+
+
+const name = randomSuperhero();
+
+console.log(name);
