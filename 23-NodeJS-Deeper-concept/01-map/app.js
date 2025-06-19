7a1a4bb4a8a0cd3639f817838f3a04c358f4d4f5
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2025-06-19 13:56:29 +0200

    Refactor map example in app.js

--- 23-NodeJS-Deeper-concept/01-map/app.js
+++ 23-NodeJS-Deeper-concept/01-map/app.js
@@ -26,20 +26,30 @@ const people = [
 // does not change the size of original array (unlike filter)
 // uses values from original array when making new one
 
-const getAges = (person) => person.age * 2;
 
-const ages = people.map(getAges);
-console.log(ages);
+// const getAges = (person)=>{
+//   return person.age *2;
+// }
 
-const newPeople = people.map((item) => {
-  return {
-    firsName: item.name.toUpperCase(),
+// const ages = people.map(getAges)
+// console.log(ages);
+
+
+const newPeople = people.map((item)=>{
+  return{
+    firstName: item.name.toUpperCase(),
     oldAge: item.age + 20,
-  };
+  }
 });
-console.log(newPeople);
 
-const names = people.map((person) => `<h2>${person.name}</h2>`);
-const result = document.querySelector('#result');
+// console.log(newPeople);
 
-result.innerHTML = names.join('');
+const names = people.map((person)=>`<h2>${person.name}</h2>`);
+
+const res = document.getElementById('result')
+
+
+res.innerHTML = names.join('');
+// names.forEach(element => {
+//   res.innerHTML += element;
+// });
