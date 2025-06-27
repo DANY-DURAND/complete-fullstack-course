75d7f94dae9689851ad760422fa2b66909c152fb
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2025-06-27 17:19:46 +0200

    Refactor app.js to replace array example with object usage and enhance state management

--- 23-NodeJS-Deeper-concept/03-dynamic-object-keys/app.js
+++ 23-NodeJS-Deeper-concept/03-dynamic-object-keys/app.js
@@ -1,13 +1,60 @@
-const person = ['hello', 'great'];
+// const person = ['hello', 'great'];
 
-const testPerson = person.map(function (item) {
-  console.log(item);
-});
+// const testPerson = person.map(function (item) {
+//   console.log(item);
+// });
 
-const newPerson = person.map(function (item) {
-  if (item === 'hello') {
-    return item.toUpperCase();
-  }
-  return item;
-});
-console.log(newPerson);
+// const newPerson = person.map(function (item) {
+//   if (item === 'hello') {
+//     return item.toUpperCase();
+//   }
+//   return item;
+// });
+// console.log(newPerson);
+
+
+const person = {
+  name: 'john',
+}
+
+console.log(person.name);
+
+person.age = 25;
+
+console.log(person);
+
+
+const items = {
+  'featured-items': ['item1', 'item2']
+}
+
+console.log(items["featured-items"]);
+
+console.log(person['name']);
+
+
+let appState = "loading";
+appState = 'error'
+const keyName = 'computer'
+const app = {
+  [appState]:true
+}
+
+app[keyName] = 'apple'
+
+console.log(app);
+
+const state = {
+  loading: true,
+  name :'',
+  job : ''
+}
+
+function updateState(key, value) { 
+  state[key] = value
+ }
+
+ updateState('name', 'John');
+
+ console.log(state);
+ 
