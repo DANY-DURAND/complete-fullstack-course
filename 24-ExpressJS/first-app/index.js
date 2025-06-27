6a67ddd2044f0dd8feed4ad64309e36765e66210
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2025-06-27 17:41:33 +0200

    Restructure project folder

--- 24-ExpressJS/first-app/index.js
+++ 24-ExpressJS/first-app/index.js
@@ -4,6 +4,15 @@ const app = express();
 
 const port = 3000;
 
+
+app.get('/', (req,res)=>{
+    res.send('Hello, World!');
+});
+
+app.get(`/hello/`,(req,res)=>{
+    res.send(`Hello, ${req.data}!`);
+});
+
 app.listen(port,()=>{
     console.log("Server running on port:", port);
     
