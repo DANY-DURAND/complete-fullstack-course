159c03af1be8a8689822696757b3553f18eecc0b
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2025-06-28 20:37:24 +0200

    Add morgan and rotating-file-stream for enhanced logging in middleware

--- 24-ExpressJS/middleware/index2.js
+++ 24-ExpressJS/middleware/index2.js
@@ -1,7 +1,29 @@
 import express from "express";
+import morgan from "morgan";
+// import fs from 'fs';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+ 
+// import * as rfs from 'rotating-file-stream';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+
+
+// var accessLogStream = fs.createWriteStream( __dirname +'access.log', {flags: 'a'});
+// var accessLogStream = rfs.createStream( 'access.log',{
+//   interval: '1d',
+//   path: `${__dirname}log`
+// });
+
+// app.use(morgan('combined', { stream: accessLogStream }));
+
+app.use((req, res, next)=>{
+  console.log(`Request method: ${req.method}`);
+  next();
+})
 
 app.get("/", (req, res) => {
   res.send("Hello");
