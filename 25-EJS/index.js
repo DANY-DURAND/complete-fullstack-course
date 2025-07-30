829513ed7ce6463af1caf51c25748d2208f94f1a
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2025-07-30 12:49:08 +0200

    Add ejs challenge for tags

--- 25-EJS/index.js
+++ 25-EJS/index.js
@@ -4,22 +4,28 @@ const app = express();
 
 const port = 3000;
 
+let bowl = ['Apples', 'Oranges', "Pears"];
+
 app.get('/', (req, res)=>{
 
-    const today = new Date("June 28, 2025");
-    const day = today.getDay();
-    let type = "a week day";
-    let adv = "it's time to work hard.";
+    // const today = new Date("June 28, 2025");
+    // const day = today.getDay();
+    // let type = "a week day";
+    // let adv = "it's time to work hard.";
 
-    if (day === 0 || day ===6){
-        type = "the weekend";
-        adv = "it's time to have fun";
-    }
+    // if (day === 0 || day ===6){
+    //     type = "the weekend";
+    //     adv = "it's time to have fun";
+    // }
 
-    res.render('index.ejs',{
-        dataType: type,
-        advice: adv
-    });
+    // res.render('index.ejs',{
+    //     dataType: type,
+    //     advice: adv
+    // });
+
+    res.render("index.ejs", {fruits: bowl});
+
+
 });
 
 app.listen(port, ()=>{
