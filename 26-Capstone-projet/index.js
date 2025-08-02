709ac828f4dd1f6fc8075724ef3dedc708c3a3c6
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2025-08-02 12:02:16 +0200

    Add logic for fetching data

--- 26-Capstone-projet/index.js
+++ 26-Capstone-projet/index.js
@@ -1,2 +1,33 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+
+
+const app = express();
+const port = 3000;
+
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>{
+    res.render('index.ejs', {data})
+});
+
+app.get('/about', (req, res)=>{
+    res.render('about.ejs')
+})
+
+app.listen(port,()=>{
+    console.log(`Listening to port: ${port}`);
+})
+
+const data = [
+    {
+        "title":"Hello, World!",
+        "content": "I am learning programming for a career change. Here is what I have learnt so far."
+    },
+    {
+        "title":"Hello, World!",
+        "content": "I am learning programming for a career change. Here is what I have learnt so far."
+    }
+]
