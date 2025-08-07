2552bdf9b9b275a53430705268375ed9df920d4d
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2025-08-07 15:12:04 +0200

    Add axios logic for handling API calls

--- 28-APIs/5.1_Testing_Native_NodeJS_Request/index.js
+++ 28-APIs/5.1_Testing_Native_NodeJS_Request/index.js
@@ -1,43 +1,54 @@
 import https from 'https';
-import express from 'express';
+import express, { response } from 'express';
 // import { hostname } from 'os';
 
+import axios from 'axios';
 const app = express();
 
 const port = 3000;
 
 
-app.get("/",()=>{
-    const options = {
-        hostname:"bored-api.appbrewery.com",
-        path: "/random",
-        method: "GET",
-    };
-    const request = https.request(options, (response)=>{
-        let data = "";
-        response.on('data', (chunk)=>{
-            data = chunk;
-        });
+// app.get("/",()=>{
+//     const options = {
+//         hostname:"bored-api.appbrewery.com",
+//         path: "/random",
+//         method: "GET",
+//     };
+//     const request = https.request(options, (response)=>{
+//         let data = "";
+//         response.on('data', (chunk)=>{
+//             data = chunk;
+//         });
 
-        response.on("end", ()=>{
-            try{
-                const result = JSON.parse(data);
-                res.render("index.ejs", {activity: result});
-            }catch{
-                console.error(`Failed to parse repsonse: ${error.message}`);
-                res.status(500).send('Failed to fetch activity. Please try again.');
-            }
-        });
+//         response.on("end", ()=>{
+//             try{
+//                 const result = JSON.parse(data);
+//                 res.render("index.ejs", {activity: data});
+//             }catch{
+//                 console.error(`Failed to parse repsonse: ${error.message}`);
+//                 res.status(500).send('Failed to fetch activity. Please try again.');
+//             }
+//         });
 
-    })
+//     })
 
-    request.on('error', (error)=>{
-        console.error(`Failed to make request: ${error.message}`);
+//     request.on('error', (error)=>{
+//         console.error(`Failed to make request: ${error.message}`);
+//         res.status(500).send('Failed to fetch activity. Please try again.');
+//     });
+
+//     request.end();
+// })
+
+app.get("/", async (req,res) => {
+    try {
+        const repsonse = await axios.get("https://bored-api.appbrewery.com/random");
+        res.render('index.ejs', {activity: response.data}) 
+    } catch (error) {
+        console.error(`Failed to make requests: ${error.message}`);
         res.status(500).send('Failed to fetch activity. Please try again.');
-    });
-
-    request.end();
-})
+    }
+});
 
 app.listen(port, ()=>{
     console.log(`Server running on port: ${port}`); 
